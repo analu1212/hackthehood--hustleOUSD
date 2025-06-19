bad6e26b284af6cb073d7bce5a29a2d2b8beb3cf
Ana Lu <analu@Anas-MacBook-Pro.local>
2025-06-18 17:05:47 -0700

    finished lab 2

---
+++ lab2.py
@@ -0,0 +1,20 @@
+lab2.py
+# Lab 2 python 
+## step 1
+Ana = "Ana Cuenca"
+
+## step 2
+nickname = 'Anny'
+
+## step 3
+age = 15
+
+## step 4
+used_python = "false"
+
+## step 5
+hobbies = ["dance,reading, cheerleading"]
+
+## step 6
+favorite_things = {"move": "frozen", 'dance': 'hip pop', 'animal': 'bunnies'}
+
