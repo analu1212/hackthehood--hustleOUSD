c627b5b7e3dc99c08096ccb322e590530d086031
analu1212 <cuencaanaluisa12@gmail.com>
2025-07-30 16:46:32 -0700

    lab4.py

---
+++ lab4.py
@@ -0,0 +1,23 @@
+#checking = 'yes'
+
+#while continue_checking = "yes"
+#print("what is your age?")
+#user_input = input ()
+#if int(user_input) >= 18:
+ #    print("yes you can vote")
+ #else:
+  #      print("you can't vote")
+#print("would you like to check another age?")
+#use_input2 = input()
+#checking = user_input2
+
+
+
+inventory =  ["tnt", "glass", "netherlite", ""]
+list_length = len(inventory)
+
+for i in len(inventory):
+    if i == "waxed lightly weathered chiseled copper stairs":
+       print("why do you have this in your inventory?")
+    elif i == "tnt":
+       print("go bomb jhhny house")
