03a59ac3678de2d54572edeaf4f964421303435b
analu1212 <cuencaanaluisa12@gmail.com>
2025-07-30 16:57:17 -0700

    debugging_activity.py

---
+++ debugging_activity.py
@@ -0,0 +1,22 @@
+#debugging activity  Ana Cuenca
+
+
+# Incorrect:
+numbers = [1, 2, 3, 4, 5]
+for i in range(len(numbers)):
+   print(numbers[i+1])
+
+# Incorrect:
+def calculate_area(radius)
+   area = 3.14 * radius ** 2
+   return area
+
+# Incorrect:
+def is_even(number):
+   if number % 2 == 0
+       return True
+   else
+       return False
+ 
+print(is_even(4))
+print(is_even(7))
